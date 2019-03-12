444106da130fed507be37642437453c367124e58
Mark Przepiora <mark.przepiora@gmail.com>
2019-03-11 18:55:11 -0700

    get rspec working

--- spec/crave_spec.rb
+++ spec/crave_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Crave do
   it "has a version number" do
-    expect(Crave::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    Crave::VERSION.should_not be_nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,6 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
 
   config.expect_with :rspec do |c|
-    c.syntax = :expect
+    c.syntax = :should
   end
 end
